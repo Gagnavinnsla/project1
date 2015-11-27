5a9680a8e5eba87d84ce5521b9e9085c134afc23
omarsindri <omarsindri@hotmail.com>
2015-11-27 15:00:59 +0000

    hve vesen

--- project.py
+++ project.py
@@ -1,22 +1,16 @@
 x = int(input("Sladu inn tolu: "))
 
 if x < 0:
-<<<<<<< HEAD
 x = 0
     print('Villa! Bannad ad sla inn neikvaeda tolu')
-=======
     x = 0
     print('Negative changed to zero')
->>>>>>> b8b469f1e0e6de6a312bec49946f9e7cdea8bbea
 elif x == 0:
     print('Null')
 elif x == 1:
     print('Solo')
 else:
     print('More')
-
-<<<<<<< HEAD
 print ("Hér var ég")
-=======
+
 print("Hvaða fucking bail er þetta Fannar?")
->>>>>>> b8b469f1e0e6de6a312bec49946f9e7cdea8bbea
