e300d1ba5666bd3c1774a404b1b1e8b50dadb1ac
omarsindri <omarsindri@hotmail.com>
2015-11-27 15:03:44 +0000

    eit4hh

--- project.py
+++ project.py
@@ -1,10 +1,6 @@
 x = int(input("Sladu inn tolu: "))
 
 if x < 0:
-<<<<<<< HEAD
-=======
-
->>>>>>> e076392ef6febcc52fad93fc2fc18622048773b5
 x = 0
     print('Villa! Bannad ad sla inn neikvaeda tolu')
     x = 0
@@ -15,12 +11,8 @@ elif x == 1:
     print('Solo')
 else:
     print('More')
-<<<<<<< HEAD
-print ("Hér var ég")
-
-=======
-
 
 print ("Hér var ég")
->>>>>>> e076392ef6febcc52fad93fc2fc18622048773b5
+
+print ("Hér var ég")
 print("Hvaða fucking bail er þetta Fannar?")
