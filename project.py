47ba98357ab42e1c95dce1de49c42e6f2a2c3438
omarsindri <omarsindri@hotmail.com>
2015-11-27 14:36:02 +0000

    What in the actual fuck?

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 x = int(input("Please enter an integer: "))
 
 if x < 0:
-x = 0
+    x = 0
     print('Negative changed to zero')
 elif x == 0:
     print('Zero')
@@ -9,3 +9,5 @@ elif x == 1:
     print('Single')
 else:
     print('More')
+
+print("Hvaða fucking bail er þetta Fannar?")
