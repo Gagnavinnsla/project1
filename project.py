e076392ef6febcc52fad93fc2fc18622048773b5
johannesh10 <johanneshilmarsson@gmail.com>
2015-11-27 14:59:37 +0000

    Breyting

--- project.py
+++ project.py
@@ -1,13 +1,11 @@
 x = int(input("Sladu inn tolu: "))
 
 if x < 0:
-<<<<<<< HEAD
+
 x = 0
     print('Villa! Bannad ad sla inn neikvaeda tolu')
-=======
     x = 0
     print('Negative changed to zero')
->>>>>>> b8b469f1e0e6de6a312bec49946f9e7cdea8bbea
 elif x == 0:
     print('Null')
 elif x == 1:
@@ -15,8 +13,6 @@ elif x == 1:
 else:
     print('More')
 
-<<<<<<< HEAD
+
 print ("Hér var ég")
-=======
 print("Hvaða fucking bail er þetta Fannar?")
->>>>>>> b8b469f1e0e6de6a312bec49946f9e7cdea8bbea
