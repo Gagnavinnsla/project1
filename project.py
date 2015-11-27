e72951826aecac90c56626e929bc69044100eab2
johannesh10 <johanneshilmarsson@gmail.com>
2015-11-27 14:51:32 +0000

    Updating file

--- project.py
+++ project.py
@@ -1,11 +1,13 @@
-x = int(input("blessadur: "))
+x = int(input("Sladu inn tolu: "))
 
 if x < 0:
 x = 0
-    print('Negative changed to zero')
+    print('Villa! Bannad ad sla inn neikvaeda tolu')
 elif x == 0:
     print('Null')
 elif x == 1:
     print('Solo')
 else:
     print('More')
+
+print ("Hér var ég")
