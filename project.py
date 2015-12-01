a7d995fa444dce7b80876046d5535342841a82bb
omarsindri <omarsindri@hotmail.com>
2015-12-01 15:16:33 +0000

    Sigh

--- project.py
+++ project.py
@@ -18,4 +18,14 @@ def openfile(Nameoffile):
 	return Header,Vektor
 
 Header1,Vektor1=openfile('SAM04101.csv')
-print(Header1)
+Header2,Vektor2=openfile('mannfjoldi.csv')
+
+Ar1=Header1.index('Ár')
+Ar2=Header2.index('Ár')
+for i in range(len(Vektor1)):
+	for j in range(len(Vektor2)):
+		if Vektor1[i][Ar1] == Vektor2[j][Ar2]:
+			Index1=Vektor1[Ar1][i].index()
+			print('True')
+print(Vektor1[Index1][Ar1])
+
