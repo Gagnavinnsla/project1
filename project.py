8575f58758a54a1d8d8af88ac7ddb538677ffe60
omarsindri <omarsindri@hotmail.com>
2015-12-01 15:48:56 +0000

    Buid ad finna range

--- project.py
+++ project.py
@@ -22,10 +22,26 @@ Header2,Vektor2=openfile('mannfjoldi.csv')
 
 Ar1=Header1.index('Ár')
 Ar2=Header2.index('Ár')
-for i in range(len(Vektor1)):
-	for j in range(len(Vektor2)):
-		if Vektor1[i][Ar1] == Vektor2[j][Ar2]:
-			Index1=Vektor1[Ar1][i].index()
-			print('True')
-print(Vektor1[Index1][Ar1])
+class Found(Exception): pass
+Boolian=True
+counter=0
+try:
+	for i in range(len(Vektor1)):
+		for j in range(len(Vektor2)):
+			if Vektor1[i][Ar1] == Vektor2[j][Ar2]:
+				Index1=i
+				Index2=j
+				raise Found
+except Found:
+	print('Worked')
 
+try:
+	while Vektor1[Index1+counter][Ar1] == Vektor2[Index2+counter][Ar2]:
+		counter+=1
+except IndexError:
+	pass
+print(Vektor1[Index1][Ar1])
+print(Vektor2[Index2][Ar2])
+print('--------------------------')
+for i in range(counter):
+	print(Vektor1[Index1+i][Ar1])
