4226b6a0650aa658074d8497c8f69adcead0d89c
David Halldorsson <davidhal14@ru.is>
2015-12-02 12:30:40 +0000

    taka úr project1
    function frá skjali1

--- func.py
+++ func.py
@@ -1,1 +1,38 @@
+def openfile(Nameoffile):
+	Vektor=[]
+	Boolian=True
+	with open(Nameoffile) as csvfile:
+		File = csv.reader(csvfile, delimiter=';')
 
+		for row in File:
+				if Boolian:
+					Header=row
+					Boolian=False
+				else:
+						Vektor.append((row))
+		Ar=Header.index('Ár')
+	return Header,Vektor,A
+
+
+def Veljaflokk(Header,Vektor,Index,counter):
+	Boolian=True
+	while Boolian==True:
+		try:
+			[print(i,end=", ") for i in Header]
+			indexinput=Header.index(input("\nVeldu flokk:\n"))
+			Boolian=False
+		except ValueError:
+			print("\nReyndu aftur\n")
+			continue
+	list1=[]
+	listdeleted=[]
+	i=0
+	while i<counter:
+		if Vektor[Index+i][indexinput].isdigit():
+			list1.append(Vektor[Index+i][indexinput])
+		else:
+			listdeleted.append(Vektor[Index+i])
+			del Vektor[Index+i]
+			counter-=1
+		i+=1
+	return list1,counter,listdeleted
