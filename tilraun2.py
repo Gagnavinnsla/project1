4be5e905a44d45e9dd01cfd7048b84403b91710e
darthvader17 <fannara14@ru.is>
2015-12-04 14:27:15 +0000

    fannar

--- tilraun2.py
+++ tilraun2.py
@@ -1,49 +1,32 @@
-from pandas import *
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np; import pandas as pd; import matplotlib.pyplot as plt; import pylab
 
 def Lesaskra(nafn):
-	settings = {}
-	settings['encoding'] = 'utf-8'
-	settings['sep'] = ';'
-	settings['dayfirst'] = True
+	settings = {'encoding':'utf-8', 'sep':';', 'dayfirst':True}
 	skjal = pd.read_csv(nafn, **settings)
 	skjal.set_index('Ár',inplace=True)
-	skjal = skjal.apply(pd.to_numeric, errors='coerce') 
-	#gæti þurft að update-a pandas til að pd.to_numeric virkar
-	skjal=skjal.dropna(axis=1,how='all')
+	skjal = skjal.apply(pd.to_numeric, errors='coerce').dropna(axis=1,how='all')  #gæti þurft að update-a pandas til að pd.to_numeric virkar
 	return skjal
 
 skjal1=Lesaskra('mannfjoldi.csv')
 skjal2=Lesaskra('Afbrot.csv')	
-skjal = pd.concat([skjal1,skjal2],axis=1, join_axes=[skjal1.index]).dropna()
+skjal = pd.concat({'mannfjoldi.csv': skjal1,'Afbrot.csv': skjal2},axis=1, join_axes=[skjal1.index]).dropna()
 
-d = {}
-d['Staðalfrávik'] = np.std(skjal, axis=0)
-d['Meðaltal'] = np.mean(skjal, axis=0)
-d['Miðgildi'] = np.median(skjal, axis=0)
-d['Max'] =np.max(skjal, axis=0)
-d['Min']=np.min(skjal, axis=0)
-df = pd.DataFrame(d)
-#saman =pd.concat([df1,df2])
-print(df)
+df = pd.DataFrame({'Staðalfrávik':np.std(skjal),'Meðaltal':np.mean(skjal),'Miðgildi':np.median(skjal,axis=0),'Max':np.max(skjal), 'Min':np.min(skjal)})
+
+print(skjal.columns)
+"""
+x1 = input("\nVeldu skrá: ")
+x2 = input("\nVeldu flokk: ")
+y1 = input("\nVeldu skrá: ")
+y2 = input("\nVeldu samanburðarflokk: ")
+
+fylgni = np.corrcoef(skjal[x1][x2].pct_change(), skjal[y1][y2].pct_change())[0][1]
+
+
+skjal[x1][x2].pct_change().plot()
+skjal[y1][y2].pct_change().plot()
+plt.show()
+
 skjal.to_csv('Nidurstodur.csv')
 df.to_csv('Nidurstodur2.csv')
-
 """
-[print(i,end=", ") for i in skjal[0:colfjol]]
-x = input("\nVeldu flokk: ")
-
-stdev = np.std(skjal[x])
-meðaltal = np.mean(skjal[x])
-miðgildi = np.median(skjal[x])
-"""
-#1 = [i.strip().replace(' ','_') for i in skjal[0:colfjol]]
-
-#-------------------------Operation Tilgáta-----------------------------#
-
-
-
-
-
