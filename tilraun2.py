c25740a5e79cf54a0063266c029b7e80f9b63dce
omarsindri <omarsindri@hotmail.com>
2015-12-04 15:44:07 +0000

    Almost final!

--- tilraun2.py
+++ tilraun2.py
@@ -7,26 +7,24 @@ def Lesaskra(nafn):
 	skjal = skjal.apply(pd.to_numeric, errors='coerce').dropna(axis=1,how='all')  #gæti þurft að update-a pandas til að pd.to_numeric virkar
 	return skjal
 
-skjal1=Lesaskra('mannfjoldi.csv')
-skjal2=Lesaskra('Afbrot.csv')	
+Valinskra=input('Veldu skrá til að bera saman við "Afbrot.csv":')
+skjal1=Lesaskra('Afbrot.csv')
+skjal2=Lesaskra(Valinskra)	
 skjal = pd.concat({'mannfjoldi.csv': skjal1,'Afbrot.csv': skjal2},axis=1, join_axes=[skjal1.index]).dropna()
-
 df = pd.DataFrame({'Staðalfrávik':np.std(skjal),'Meðaltal':np.mean(skjal),'Miðgildi':np.median(skjal,axis=0),'Max':np.max(skjal), 'Min':np.min(skjal)})
 
-print(skjal.columns)
-"""
+[print(i,end="; ") for i in skjal]
 x1 = input("\nVeldu skrá: ")
-x2 = input("\nVeldu flokk: ")
+x2 = input("\nVeldu undirflokk: ")
 y1 = input("\nVeldu skrá: ")
 y2 = input("\nVeldu samanburðarflokk: ")
 
-fylgni = np.corrcoef(skjal[x1][x2].pct_change(), skjal[y1][y2].pct_change())[0][1]
-
-
-skjal[x1][x2].pct_change().plot()
-skjal[y1][y2].pct_change().plot()
+plt.style.use('ggplot')
+df2=pd.DataFrame({x2:skjal[x1][x2].pct_change(),y2:skjal[y1][y2].pct_change()}).dropna(axis=0)
+print('Fylgni valinna gagna er: ', np.corrcoef(df2[x2],df2[y2])[0][1])
+df2.plot()
 plt.show()
 
-skjal.to_csv('Nidurstodur.csv')
-df.to_csv('Nidurstodur2.csv')
-"""
+skjal.to_csv('Gögn.csv')
+df.to_csv('Tolfraedi.csv')
+df2.to_csv('breytingFlokka.csv')
