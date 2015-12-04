0bb7c333e893cf5cfb409d3ba4d26e741aecd568
omarsindri <omarsindri@hotmail.com>
2015-12-04 12:15:40 +0000

    da

--- tilraun2.py
+++ tilraun2.py
@@ -22,18 +22,15 @@ skjal = pd.concat([skjal1,skjal2],axis=1, join_axes=[skjal1.index]).dropna()
 d = {}
 d['Staðalfrávik'] = np.std(skjal, axis=0)
 d['Meðaltal'] = np.mean(skjal, axis=0)
-#skiptir ekki máli hvort maður noti skjal.apply eða np.brah(skjal) nema fyrir median
 d['Miðgildi'] = np.median(skjal, axis=0)
 d['Max'] =np.max(skjal, axis=0)
 d['Min']=np.min(skjal, axis=0)
 df = pd.DataFrame(d)
-
 #saman =pd.concat([df1,df2])
 print(df)
 skjal.to_csv('Nidurstodur.csv')
 df.to_csv('Nidurstodur2.csv')
-#print(skjal1[,"\n",skjal2['Alls'])
-#print(skjal[-25:])
+
 """
 [print(i,end=", ") for i in skjal[0:colfjol]]
 x = input("\nVeldu flokk: ")
@@ -42,7 +39,7 @@ stdev = np.std(skjal[x])
 meðaltal = np.mean(skjal[x])
 miðgildi = np.median(skjal[x])
 """
-s1 = [i.strip().replace(' ','_') for i in skjal[0:colfjol]]
+#1 = [i.strip().replace(' ','_') for i in skjal[0:colfjol]]
 
 #-------------------------Operation Tilgáta-----------------------------#
 
